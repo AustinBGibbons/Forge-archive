37014b9c75c47dfecbc35534ec901a544f423318
AustinBGibbons <gibbons4@stanford.edu>
2013-05-05 17:35:08 -0700

    StringWrangling

---
+++ apps/StringWrangler/src/HelloWorld.scala
@@ -0,0 +1,20 @@
+import stringwrangler.compiler._
+import stringwrangler.library._
+import stringwrangler.shared._
+
+object HelloWranglerCompiler extends StringWranglerApplicationCompiler with HelloWrangler 
+object HelloWranglerInterpreter extends StringWranglerApplicationInterpreter with HelloWrangler 
+
+trait HelloWrangler extends StringWranglerApplication { 
+  def main() = {
+    println("hello world")    
+    
+    val col = Column(3, "3")
+
+    col(0) = "0"
+    col(2) = "2"
+
+    println(col(0))
+    println(col(1))
+  }
+}
--- src/examples/StringWrangler.scala
+++ src/examples/StringWrangler.scala
@@ -3,14 +3,14 @@ package examples
 
 import core.{ForgeApplication,ForgeApplicationRunner}
 
-object OptiWranglerDSLRunner extends ForgeApplicationRunner with OptiWranglerDSL
+object StringWranglerDSLRunner extends ForgeApplicationRunner with StringWranglerDSL
 
-trait OptiWranglerDSL extends ForgeApplication with ScalaOps {
+trait StringWranglerDSL extends ForgeApplication with ScalaOps {
   /**
    * The name of your DSL. This is the name that will be used in generated files,
    * package declarations, etc.
    */
-  def dslName = "OptiWrangler"
+  def dslName = "StringWrangler"
   
   /**
    * The specification is the DSL definition (types, data structures, ops)
@@ -30,58 +30,57 @@ trait OptiWranglerDSL extends ForgeApplication with ScalaOps {
   
   def addWranglerOps() {            
     // generic type parameters we will use 
-     val T = tpePar("T") 
+    // val T = tpePar("T") 
     // val R = tpePar("R")
     
-    val Table = tpe("Table", T) 
+    val Column = tpe("Column") 
     //val SArray = tpeInst(MArray, MString)
 
     val SArray = tpeInst(MArray, MString)
-    val SSArray = tpeInst(MArray, SArray)
 
-    val TableOps = withTpe (Table)
-    TableOps {                  
+    val ColumnOps = withTpe (Column)
+    ColumnOps {                  
       // data fields     
-      data(("_data", SSArray), ("_width", MInt) /*, ("_header", Map[String, Int])*/, ("_name", MString))
+      data(("_data", SArray), ("_width", MInt) /*, ("_header", Map[String, Int])*/, ("_name", MString))
 
       // allocation
       // todo test wrong experiment wip dev how to have defaults
-      op (Table) ("apply", static, T, (MInt, MString), Table, effect = mutable) implements allocates(Table, ${null}, ${$0}, ${$1})
+      op (Column) ("apply", static, Nil, (MInt, MString), Column, effect = mutable) implements allocates(Column, ${ array_empty[String]($0)}, ${$0}, ${$1})
       
       // getters and setters
-      "data" is (compiler, Nil, SSArray) implements getter(0, "_data")
+      "data" is (compiler, Nil, SArray) implements getter(0, "_data")
       "length" is (infix, Nil, MInt) implements composite ${array_length(data($self))}
       
       // data ops             
-      "apply" is (infix, (MInt), SArray) implements composite ${ array_apply(data($self), $1) }                        
+      "apply" is (infix, (MInt), MString) implements composite ${ array_apply(data($self), $1) }                        
       // example named arg
-      "update" is (infix, (("i",MInt),("e",SArray)), MUnit, effect = write(0)) implements composite ${
+      "update" is (infix, (("i",MInt),("e",MString)), MUnit, effect = write(0)) implements composite ${
         array_update(data($self), $i, $e)
       }
 
-      parallelize as ParallelCollection(SArray,
+      parallelize as ParallelCollection(MString,
        lookupOverloaded("apply",1),
        lookup("length"),
        lookupOverloaded("apply",0),
        lookup("update") 
       )            
 
-      /* OptiWrangler Ops */
-      "copy" is (compiler, (SSArray), Table) implements setter(0, "_data", quotedArg(1)) 
+      /* StringWrangler Ops */
+/*
+      "copy" is (compiler, (SSArray), Column) implements setter(0, "_data", quotedArg(1)) 
 
   //"mapWrap" is (compiler, (MString ==> MString, MInt), SSArray) implements map((SArray, SArray), 0, 
   //${ e => $1(array_apply(data($self), $2)) }
-/*
       "mapWrap" is (compiler, (MString, MInt), SSArray) implements map((SArray, SArray), 0, 
         ${ e => $e }
       )
-*/      
+  */    
 /*
-      "map" is (compiler, (MString ==> MString, MInt), Table) implements composite ${copy(mapWrap($0, $1, $2))}
+      "map" is (compiler, (MString ==> MString, MInt), Column) implements composite ${copy(mapWrap($0, $1, $2))}
 */
-      //"cut" is (infix, (MString, MInt), Table) implements composite ${ map(_.replaceFirst($1, $2)) }
+      //"cut" is (infix, (MString, MInt), Column) implements composite ${ map(_.replaceFirst($1, $2)) }
 
-      "potato" is (compiler, (tpe("ForgeArray", T)), MUnit) implements single ${}
+      //"potato" is (compiler, (MInt), MUnit) implements single ${}
     }                    
                                         
     ()    
