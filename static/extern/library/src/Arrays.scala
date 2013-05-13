038de27ad4853857d6a8efb5fdc074c058047fc4
AustinBGibbons <gibbons4@stanford.edu>
2013-05-12 23:15:22 -0700

    adding array ops

--- static/extern/library/src/Arrays.scala
+++ static/extern/library/src/Arrays.scala
@@ -21,8 +21,29 @@ trait ForgeArrayWrapper extends HUMAN_DSL_NAMEBase {
     = __arg0.length
   def array_asimmutable[T:Manifest](__arg0: Rep[ForgeArray[T]])(implicit __imp0: SourceContext): Rep[ForgeArray[T]]
     = __arg0
+
+  def array_map[A:Manifest,B:Manifest](a: Rep[DeliteArray[A]], f: Rep[A] => Rep[B]): Rep[DeliteArray[B]]
+    = a.map(f)
+  // note - I changed to zip to match Forge
+  def array_zip[A:Manifest,B:Manifest,R:Manifest](x: Rep[DeliteArray[A]], y: Rep[DeliteArray[B]], f: (Rep[A],Rep[B]) => Rep[R]): Rep[DeliteArray[R]]
+    = x.zip(y).map(f)
+  def array_reduce[A:Manifest](x: Rep[DeliteArray[A]], f: (Rep[A],Rep[A]) => Rep[A], zero: Rep[A]): Rep[A]
+    = x.reduce(f)
+  def array_filter[A:Manifest](x: Rep[DeliteArray[A]], f: Rep[A] => Rep[Boolean]): Rep[DeliteArray[A]]
+    = x.filter(f)
+  def array_mkstring[A:Manifest](a: Rep[DeliteArray[A]], del: Rep[String]): Rep[String]
+    = __arg0
+  def array_union[A:Manifest](lhs: Rep[DeliteArray[A]], rhs: Rep[DeliteArray[A]]): Rep[DeliteArray[A]]
+    = __arg0
+  def array_intersect[A:Manifest](lhs: Rep[DeliteArray[A]], rhs: Rep[DeliteArray[A]]): Rep[DeliteArray[A]]
+    = __arg0
+  def array_take[A:Manifest](lhs: Rep[DeliteArray[A]], n: Rep[Int])(implicit ctx: SourceContext): Rep[DeliteArray[A]]
+    = __arg0
+  def array_sort[A:Manifest](lhs: Rep[DeliteArray[A]]): Rep[DeliteArray[A]]
+    = __arg0
+  def array_range(st: Rep[Int], en: Rep[Int]): Rep[DeliteArray[Int]]
+    = __arg0
+  def array_toseq[A:Manifest](a: Rep[DeliteArray[A]]): Rep[Seq[A]]
+    = __arg0
+
 }
-
-
-
-
