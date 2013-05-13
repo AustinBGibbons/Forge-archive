e1800289478991e0eb9edc44e390b71b4da98357
AustinBGibbons <gibbons4@stanford.edu>
2013-05-13 00:04:21 -0700

    Moving to new syntax

--- src/examples/OptiWrangler.scala
+++ src/examples/OptiWrangler.scala
@@ -39,27 +39,27 @@ trait OptiWranglerDSL extends ForgeApplication with ScalaOps {
     val SArray = tpeInst(MArray, MString)
     val SSArray = tpeInst(MArray, SArray)
 
-    val TableOps = withTpe (Table)
-    TableOps {                  
-      // data fields     
-      data(("_data", SSArray), ("_width", MInt) /*, ("_header", Map[String, Int])*/, ("_name", MString))
-
+      data(Table, ("_data", SSArray), ("_width", MInt) /*, ("_header", Map[String, Int])*/, ("_name", MString))
       // allocation
       op (Table) ("apply", static, Nil, (SSArray, MInt, MString), Table, effect = mutable) implements allocates(Table, ${$0}, ${$1}, ${$2})
 
       // todo test wrong experiment wip dev how to have defaults
       op (Table) ("apply", static, Nil, (MInt, MString), Table, effect = mutable) implements allocates(Table, ${array_empty[ForgeArray[String]]($0)}, ${$0}, ${$1})
+
+
+    val TableOps = withTpe (Table)
+    TableOps {                  
       
       // getters and setters
-      "data" is (compiler, Nil, SSArray) implements getter(0, "_data")
-      "copy" is (compiler, SSArray, MUnit, effect=write(0)) implements setter(0, "_data", quotedArg(1))
-      "length" is (infix, Nil, MInt) implements composite ${array_length(data($self))}
+      "data" is (compiler, Nil :: SSArray) implements getter(0, "_data")
+      "copy" is (compiler, SSArray :: MUnit, effect=write(0)) implements setter(0, "_data", quotedArg(1))
+      "length" is (infix, Nil :: MInt) implements composite ${array_length(data($self))}
       
       // data ops             
-      "apply" is (infix, (MInt, MInt), MString) implements composite ${ array_apply(array_apply(data($self), $1), $2) }                        
-      "apply" is (infix, (MInt), SArray) implements composite ${ array_apply(data($self), $1) }                        
+      "apply" is (infix, (MInt, MInt) :: MString) implements composite ${ array_apply(array_apply(data($self), $1), $2) }                        
+      "apply" is (infix, (MInt) :: SArray) implements composite ${ array_apply(data($self), $1) }                        
       // example named arg
-      "update" is (infix, (("i",MInt),("e",SArray)), MUnit, effect = write(0)) implements composite ${
+      "update" is (infix, (("i",MInt),("e",SArray)) :: MUnit, effect = write(0)) implements composite ${
         array_update(data($self), $i, $e)
       }
 
@@ -78,20 +78,20 @@ trait OptiWranglerDSL extends ForgeApplication with ScalaOps {
 
       // Friends and Helpers
   
-      "map" is (infix, (String ==> String, MAny), Table) implements composite ${
+      "map" is (infix, (String ==> String, MAny) :: Table) implements composite ${
         val indices = getColumns($2)
         array_map(data($self), row => row)
       }
 
       // API
 
-      "cutRow" is (infix, (MInt, SArray), SArray) implements composite ${
+      "cutRow" is (infix, (MInt, SArray) :: SArray) implements composite ${
         array_map[String, String]($2, cell =>
           if($1 >= cell.size) cell
           else cell.substring(0, $1) + cell.substring($1 + 1)
         )
       }
-      "cut" is (infix, MInt, Table) implements composite ${
+      "cut" is (infix, MInt :: Table) implements composite ${
         if($1 < 0) println("Well there goes the __ neighborhood")
         copy($self, array_map[ForgeArray[String], ForgeArray[String]](data($self), row => $self.cutRow($1, row)))
         $self
@@ -99,10 +99,10 @@ trait OptiWranglerDSL extends ForgeApplication with ScalaOps {
 
       // IO - could be better
       // static?
-      "fromFile" is (infix, MString, SSArray) implements codegen ($cala, ${
+      "fromFile" is (infix, MString :: SSArray) implements codegen ($cala, ${
         scala.io.Source.fromFile($1).getLines().map(_.split(",").toArray).toArray
       }) // tpdo - use split etc. etc.
-      "tableFromFile" is (infix, MString, Table) implements composite ${ 
+      "tableFromFile" is (infix, MString :: Table) implements composite ${ 
         val d = $self.fromFile($1)
         Table(d, array_length(d), "")
       }
