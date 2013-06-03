f814e0f2f4ca21f1d496b5d439b8208e14599897
AustinBGibbons <gibbons4@stanford.edu>
2013-06-03 03:13:09 -0700

    Errenous Type issue

--- apps/MapTest/src/HelloWorld.scala
+++ apps/MapTest/src/HelloWorld.scala
@@ -12,21 +12,22 @@ trait HelloWorld extends MapTestApplication {
   def main() = {
     println("Hello, World!")
     val ms = MultiSet()
-  
+ /* 
     ms.ms_add(1.0)
     ms.ms_add(1.0)
     ms.ms_add(1.5)
-    
+   */ 
     println("True? " + ms.ms_contains(1.0))
     println("False? " + ms.ms_contains(1.2))
     
     println("0? : " + ms.ms_count(1.2))
     println("1? : " + ms.ms_count(1.5))
     println("2? : " + ms.ms_count(1.0))
-  
+  /*
     ms.ms_remove(1.0)
     println("1? : " + ms.ms_count(1.0))
     ms.ms_remove(1.0)
     println("0? : " + ms.ms_count(1.0))
+*/
   }
 }
