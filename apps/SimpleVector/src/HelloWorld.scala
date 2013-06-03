ad0f536fddf90358de9af2ed0db574f41cb500c7
AustinBGibbons <gibbons4@stanford.edu>
2013-06-03 03:55:26 -0700

    HelloSimple app with different filter sum using -t8

--- apps/SimpleVector/src/HelloWorld.scala
+++ apps/SimpleVector/src/HelloWorld.scala
@@ -17,65 +17,68 @@ trait HelloSimple extends SimpleVectorApplication {
     while (i < v1.length) {
       v1(i) = i*2
       v2(i) = i
-      println("v1(" + i + "): " + v1(i))
-      println("v2(" + i + "): " + v2(i))
+//      println("v1(" + i + "): " + v1(i))
+//      println("v2(" + i + "): " + v2(i))
       i += 1
     }
     
     // zip
     val v3 = v1+v2
     i = 0
-    println("v3 = v1+v2")
+  //  println("v3 = v1+v2")
     while (i < v3.length) {
-      println("v3(" + i + "): " + v3(i))
+    //  println("v3(" + i + "): " + v3(i))
       i += 1
     }
 
     // single
-    println("v4 = v3.slice(3,5)")
+   // println("v4 = v3.slice(3,5)")
     val v4 = v3.slice(3,5)
     i = 0
     while (i < v4.length) {
-      println("v4(" + i + "): " + v4(i))
+     // println("v4(" + i + "): " + v4(i))
       i += 1
     }    
     
     // map
-    println("v5 = v4*5")
+    //println("v5 = v4*5")
     val v5 = v4*5
     i = 0
     while (i < v5.length) {
-      println("v5(" + i + "): " + v5(i))
+      //println("v5(" + i + "): " + v5(i))
       i += 1
     }      
     
     // reduce
-    println("v5.sum:")
+    //println("v5.sum:")
     val z = v5.sum
-    println(z)
+    //println(z)
     
     // foreach
-    println("v1.pprint:")
-    v1.pprint
+   // println("v1.pprint:")
+   // v1.pprint
     
     // map,reduce,mapreduce
     val vc = v1.map(e => e+2)
-    println("vc.pprint:")
-    vc.pprint
+    //println("vc.pprint:")
+    //vc.pprint
     val vc2 = vc.mapreduce(e => e-1, (a,b) => a+b)
-    println("vc2: " + vc2)
+    //println("vc2: " + vc2)
     
     // filter
     println("v6 = v1.filter(_ < 5)")
+    v1.pprint
     val v6 = v1.filter(_ < 5)
     v6.pprint  
-    
+ 
+    println("sum: " + v6.sum)
+   
     // foo
     val fooResult = foo[Int](i => i+100, 2, 13, (a,b) => a + 5, 0.7, d => d)
     // // a0 == a1 == ... an = 107
     // // z == 0.7
     // // y = 15
-    println("fooResult: " + fooResult)
+   // println("fooResult: " + fooResult)
   }
    
 }
