b509b19622e6358a13a8c9ca6078fc708e69004b
AustinBGibbons <gibbons4@stanford.edu>
2013-06-05 05:18:58 -0700

    and disabling functions ...

--- apps/OptiWrangler/src/HelloWorld.scala
+++ apps/OptiWrangler/src/HelloWorld.scala
@@ -10,7 +10,7 @@ trait HelloWrangler extends OptiWranglerApplication {
     println("hello world")    
     val t1 = Table(args(0))
     val t2 = t1.cut("1").cut("2", 1).cutRight("3").cutRight("4", 1).cutAll("\"").cutAll("0")
-      .cut(2).cut(1, 0).cut({cell => cell.indexOf("6")})
+      .cut(2).cut(1, 0)//.cut({cell => cell.indexOf("6")})
 
     t2.toFile("/home/gibbons4/data/")
   }
